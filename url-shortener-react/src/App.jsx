63202f0afb54bf4f23f661772c17f065a041bd3d
Bhabani Shankar Barik <bhabanishankar.barik21@gmail.com>
2025-04-27 19:10:31 +0530

    App.jsx updated

--- url-shortener-react/src/App.jsx
+++ url-shortener-react/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './component/LandingPage'
 import AboutPage from './component/AboutPage'
+import NavBar from './component/NavBar'
+import Footer from './component/Footer.Jsx'
 
 function App() {
   
@@ -11,10 +13,12 @@ function App() {
   return (
     <>
      <BrowserRouter>
+     <NavBar />
       <Routes>
         <Route path='/' element={<LandingPage />}  />
         <Route path='/about' element={<AboutPage />}  />
       </Routes>
+      <Footer />
      </BrowserRouter>    
     </>
   )
