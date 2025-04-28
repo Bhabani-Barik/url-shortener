405d8e20375e4b42316cf531d32322878c01c955
Bhabani Shankar Barik <bhabanishankar.barik21@gmail.com>
2025-04-28 13:29:22 +0530

    updated

--- url-shortener-react/src/App.jsx
+++ url-shortener-react/src/App.jsx
@@ -6,6 +6,10 @@ import LandingPage from './component/LandingPage'
 import AboutPage from './component/AboutPage'
 import NavBar from './component/NavBar'
 import Footer from './component/Footer.Jsx'
+import RegisterPage from './component/RegisterPage'
+import { Toaster } from 'react-hot-toast'
+import LoginPage from './component/LoginPage.Jsx'
+import DashboardLayout from './Dashboard/DashboardLayout'
 
 function App() {
   
@@ -13,10 +17,14 @@ function App() {
   return (
     <>
      <BrowserRouter>
+     <Toaster position='bottom-center' />
      <NavBar />
       <Routes>
         <Route path='/' element={<LandingPage />}  />
         <Route path='/about' element={<AboutPage />}  />
+        <Route path='/register' element={<RegisterPage />}  />
+        <Route path='/login' element={<LoginPage />}  />
+        <Route path="/dashboard" element={<DashboardLayout />} />
       </Routes>
       <Footer />
      </BrowserRouter>    
--- url-shortener-react/src/component/LandingPage.jsx
+++ url-shortener-react/src/component/LandingPage.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Card from './Card';
 import { motion } from 'framer-motion'
 import { useNavigate } from 'react-router-dom';
+import { useStoreContext } from "../contextApi/ContextApi";
+
+
+let desc =   "Generate short, memorable links with ease using Linklytics’s intuitive interface. Share URLs effortlessly across platforms. Optimize your sharing strategy with Linklytics. Track clicks and manage your links seamlessly to enhance your online presence. Generate short, memorable links with ease using Linklytics’s intuitive interface. Share URLs effortlessly across platforms.";
 
 const LandingPage = () => {
-
-    let desc =   "Generate short, memorable links with ease using Linklytics’s intuitive interface. Share URLs effortlessly across platforms. Optimize your sharing strategy with Linklytics. Track clicks and manage your links seamlessly to enhance your online presence. Generate short, memorable links with ease using Linklytics’s intuitive interface. Share URLs effortlessly across platforms.";
-
   const navigate = useNavigate();
+  const { token } = useStoreContext();
+  console.log("TOKEN FROM LANDING PAGE: " + token);
+
 
     const dashBoardNavigateHandler = () => {
 
