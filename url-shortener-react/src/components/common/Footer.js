459d4c3af259fc206d076c8fdb67639243c73ceb
Bhabani Shankar Barik <bhabanishankar.barik21@gmail.com>
2025-05-11 21:14:43 +0530

    Delete all Frontend Code

--- url-shortener-react/src/components/common/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="bg-dark text-white text-center py-3 mt-4 fixed-bottom">
-    <small>&copy; {new Date().getFullYear()} TinyLink</small>
-  </footer>
-);
-
-export default Footer;
